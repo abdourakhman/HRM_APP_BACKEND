743cece01e256a039cb5fa1ad626e36eb0b1e416
abdourahmane NDIAYE <abdourakhman920@gmail.com>
2023-06-20 12:06:36 +0200

    obsolete

--- human/src/main/java/ma/suptech/MShuman/repositories/EmployeeRepository.java
+++ human/src/main/java/ma/suptech/MShuman/repositories/EmployeeRepository.java
@@ -3,7 +3,9 @@ package ma.suptech.MShuman.repositories;
 
 import ma.suptech.MShuman.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
 }
