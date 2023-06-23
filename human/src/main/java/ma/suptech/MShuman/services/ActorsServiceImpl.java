443bdbffdd3f8fdf83dcb53a5046a518ddbdc1af
abdourakhman <abdourakhman920@gmail.com>
2023-06-23 13:35:59 +0200

    adding  method getDistinctContracts

--- human/src/main/java/ma/suptech/MShuman/services/ActorsServiceImpl.java
+++ human/src/main/java/ma/suptech/MShuman/services/ActorsServiceImpl.java
@@ -47,7 +47,7 @@ public class ActorsServiceImpl implements ActorsService {
             .limit(20)
             .collect(Collectors.toList());
     List<Integer> telephoneNumbers = new ArrayList<>();
-    List<Status> statusList = Stream.of(Status.ACTIVE,Status.ACTIVE,Status.INACTIVE).toList();
+    List<Status> statusList = Stream.of(Status.ACTIVE,Status.ACTIVE,Status.ACTIVE,Status.INACTIVE).toList();
     String photoUrl = "https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg";
     List<Long>departmentID = Stream.of(1L,2L,3L,4L,5L,6L,8L,9L,10L,11L,12L,13L).toList();
     List<Long>jobID = Stream.of(1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,11L,12L,13L,14L,15L,16L,17L,18L,19L,20L).toList();
